358099b6ab420a660ae7e481047f8b92f808ec8b
LaurelHe1 <48665557+LaurelHe1@users.noreply.github.com>
2023-03-09 23:08:12 -0800

    add about

--- app/content.py
+++ app/content.py
@@ -91,13 +91,65 @@ tab3 = dbc.Tab(id='topic_tab', label="Sentiment Topics", children=[
 #####################
 ####### tab 4 #######
 #####################
-tab4 = dbc.Tab(label="Table", children=[
-    table
+tab4 = dbc.Tab(label="About Project", children=[
+    dbc.Row([
+        dbc.Col(
+         html.Div([
+            html.H4("Trading Strategy", className="display-6"),
+            html.Hr(className="my-2"),
+            html.P([
+                html.Div("Conventional RSI Trading Strategy:", style={"font-weight":"bold", "font-size": "20px"}), 
+                "- If 10-period RSI of the stock is below 30, buy on the next day's open ", html.Br(),
+                "- If 10-period RSI is above 40 or after 10 trading days, sell on the next day's open ", html.Br(),
+                html.Br(),
+                html.Div("Proposed Sentiment Trading Strategy:", style={"font-weight":"bold", "font-size": "20px"}),
+                "- Buy at the start of bullish market signaled by the 50-day sentiment MA crossing the 200-day sentiment MA from below ", html.Br(),
+                "- Sell at the start of bearish market signaled by the 200-day sentiment MA crossing the 50-day sentiment MA from below "
+                ],
+                style = {'margin-top': '10px'}
+            ),
+            ], className="h-100 p-5 bg-light border rounded-3",  
+        ), md=6,
+    ),
+        dbc.Col(
+         html.Div([
+            html.H4("Future Work", className="display-6"),
+            html.Hr(className="my-2"),
+            html.P([
+                "- Build Kirkland GPT from BERT next-sentence prediction ", html.Br(),
+                "- Predict stock price from sentiment (will need accurate representations of correlation, lag and distance matrix) ", html.Br(),
+                "- Automate workflow: scrape Twitter -> classify sentiments on Tweets -> alert user when to buy and sell ", html.Br(),
+                "- Improve sentiment classification accuracy by adding a neutral class ", html.Br(),
+                "- Improve sentiment trading strategy accuracy ", html.Br(),
+                "- Add more input varieties like blogs, forums, comments; Add more sentiment topics like Forex, Crypto ", html.Br(),
+                "- Find correlation/uncorrelation between topics to diversify investment portfolio "],
+                style = {'margin-top': '10px'}
+            ),
+            ], className="h-100 p-5 bg-light border rounded-3",  
+        ), md=6,
+    ),
+    ]), 
+    dbc.Row([
+        html.Div([
+            html.H4("About This Project", className="display-6"),
+            html.Hr(className="my-2"),
+            html.P(
+                'Data observed in this page utilizes a semi-supervised learning approach. '
+                'The initial training involved labeled targets and incorporated the BERT model, '
+                'which itself was trained on a vast corpus of textual data. The ultimate testing '
+                'phase involved evaluating the trained data on a diverse range of Twitter tweets '
+                'that were scraped from various topics. These results were then compared against '
+                'real-world data to assess the accuracy of the approach.',
+                style = {'margin-top': '10px'}
+            ),
+            ], className="h-100 p-5 bg-light border rounded-3",  
+        )#, md=6,
+    ])
 ])
 
 # change active tab when done testing
 tabs = dbc.Card(
-    dbc.Tabs([tab1, tab2, tab3])
+    dbc.Tabs([tab1, tab2, tab3, tab4])
     )
 
 ########################
