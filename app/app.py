85bb50d3e10637df6537f7420204d2f9653aa530
set-one <danielosetiawan@gmail.com>
2023-05-31 23:06:14 -0700

    updates

--- app/app.py
+++ app/app.py
@@ -95,4 +95,4 @@ def render_page_content(pathname):
 
 if __name__ == '__main__':
     # application.run(debug=True, host='0.0.0.0', port=8080)
-    app.run_server(debug=True, host='0.0.0.0', port=8080)
+    app.run_server(debug=False, host='0.0.0.0', port=8080)
